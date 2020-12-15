993393186fe705c3876042bbc1ed1232bc815118
Georg Wille <georg.wille@gmx.net>
2020-12-15 12:46:47 +0100

    tidied up no. 14 a bit

--- aoc_14a.py
+++ aoc_14a.py
@@ -1,31 +1,32 @@
 fin = open("input_14.txt")
 
+
 def applymask(mask, number):
     binstr = str(bin(int(number)))[2:]
     binresult = ''
-    for i,char in enumerate(mask[::-1]):
+    for i, char in enumerate(mask[::-1]):
         try:
-            bit = binstr[-(i+1)]
+            bit = binstr[-(i + 1)]
         except IndexError:
             bit = '0'
         if char == '0':
             bit = '0'
         elif char == '1':
             bit = '1'
-        binresult += bit
-    binresult = binresult[::-1]
-    return int(binresult,base=2)
+        binresult = bit + binresult
+    return int(binresult, base=2)
+
 
 memory = {}
 
 for line in fin:
     line = line.strip()
     if line.startswith('mask'):
-        mask = line.split("=")[1].strip()
+        mask = line.split("=")[1]
     elif line.startswith('mem'):
-        address = int(line[line.index('[')+1:line.index(']')])
-        value = int(line.split("=")[1].strip())
-        memory[address] = applymask(mask,value)
+        address = int(line[line.index('[') + 1:line.index(']')])
+        value = int(line.split("=")[1])
+        memory[address] = applymask(mask, value)
 
 total = 0
 
--- aoc_14b.py
+++ aoc_14b.py
@@ -1,42 +1,45 @@
 fin = open("input_14.txt")
 
+
 def applymask(mask, base_address):
     base = str(bin(int(base_address)))[2:]
     addresses = ['']
-    for i,mchar in enumerate(mask[::-1]):
+    for i, mchar in enumerate(mask[::-1]):
         try:
-            bit = base[-(i+1)]
+            bit = base[-(i + 1)]
         except IndexError:
             bit = '0'
+        l = len(addresses)
         if mchar == '0':
-            for j in range(len(addresses)):
-                addresses[j] += bit
+            for j in range(l):
+                addresses[j] = bit + addresses[j]
         elif mchar == '1':
-            for j in range(len(addresses)):
-                addresses[j] += '1'
+            for j in range(l):
+                addresses[j] = '1' + addresses[j]
         elif mchar == 'X':
             temp = []
-            for j in range(len(addresses)):
-                temp.append(addresses[j]+'1')
-                addresses[j] += '0'
+            for j in range(l):
+                temp.append('1' + addresses[j])
+                addresses[j] = '0' + addresses[j]
             addresses.extend(temp)
     # print(addresses)
     final = []
     for address in addresses:
-        final.append(int(address[::-1],base=2))
+        final.append(int(address, base=2))
     # print(final)
     return final
+
 
 memory = {}
 
 for line in fin:
     line = line.strip()
     if line.startswith('mask'):
-        mask = line.split("=")[1].strip()
+        mask = line.split("=")[1]
     elif line.startswith('mem'):
-        base_address = int(line[line.index('[')+1:line.index(']')])
-        value = int(line.split("=")[1].strip())
-        alist = applymask(mask,base_address)
+        base_address = int(line[line.index('[') + 1:line.index(']')])
+        value = int(line.split("=")[1])
+        alist = applymask(mask, base_address)
         for address in alist:
             memory[address] = value
 
@@ -45,4 +48,4 @@ total = 0
 for key in memory:
     total += memory[key]
 
-print(len(memory),total)
+print(len(memory), total)
